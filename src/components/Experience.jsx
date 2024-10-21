ff89ecea14e2cb9823203a055ef748ddc9072ab5
najm-choueiry <nachoueiry@outlook.com>
2024-10-22 02:26:49 +0300

    almost done

---
+++ src/components/Experience.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { SectionWrapper } from "../hoc";
+
+const Experience = () => {
+  return <div>Experience</div>;
+};
+
+export default SectionWrapper(Experience, "work");
